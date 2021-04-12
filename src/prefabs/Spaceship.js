03e6056ab1e814e67bf8daf3e4cf1e005c4053a3
Kushiri <Hewdraw123@gmail.com>
2021-04-11 23:18:10 -0700

    Spaceship Commit 2

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -4,7 +4,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene
         this.points = pointValue;   // store pointValue
-        this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
+        this.moveSpeed = 3;
     }
 
     update() {
@@ -12,7 +12,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
         if(this.x <= 0 - this.width) {
-            this.reset();
+            this.x = game.config.width;
         }
     }
 }
